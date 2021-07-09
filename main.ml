42f54811ad6f4bec2055e16b02a148ad5b250591
= <rapidmajk@yandex.ru>
2021-07-10 01:24:47 +0700

    it is working!

--- main.ml
+++ main.ml
@@ -7,109 +7,92 @@ let input = [
       [1; 0; 0; 0; 0; 1];
 ]
 
-let add_unique lst value =
-      if List.mem value lst
-      then lst
-      else value :: lst
+let size = 6
+let size_ = 7
+let bound = 5
 
-let merge_lists = List.fold_left add_unique
-
-module FlatGraph = struct
-      type index = int
-
+module ListGraph = struct
       type t =
+            | Node of int list * t
             | End
-            | Node of index * index list * t
 
-      let root = 999
+      let empty = Node ([], End)
 
-      let empty = Node (1, [root], End)
+      let tuple_mem (a, b) list =
+            (List.mem a list) || (List.mem b list)
 
-      let rec add parent child = function
-            | End -> Node (parent, [child], End)
-            | Node (value, children, next) -> (
-                  match value - parent with
-                  | 0 -> Node (value, add_unique children child, next)
-                  | diff when diff > 0 -> Node (value, children, add parent child next)
-                  | _ -> Node (parent, [child], Node(value, children, next))
-            )
+      let rec add (left, right) acc = function
+            | End -> Node (left :: right :: acc, End)
+            | Node (items, next) ->
+                  if tuple_mem (left, right) items
+                  then add (left, right) (items @ acc) next
+                  else Node (items, add (left, right) acc next)
 
-      let rec list_of_graph acc = function
-            | End -> acc
-            | Node (value, children, next) ->
-                  let acc =
-                        if List.mem value acc
-                        then merge_lists List.(sort (fun a b -> b - a) children) acc
-                        else acc
-                  in
-                  list_of_graph acc next
+      let id = function
+            | Node (items, next) -> Node (items, next)
+            | End -> End
+
+      let rec last_items = function
+            | End -> []
+            | Node (items, End) -> items
+            | Node (_items, next) -> last_items next
 end
 
-let int_of_pos (x, y) =
-      if x < 0 || x > 5 || y < 0 || y > 5
-      then FlatGraph.root
-      else x + y * 6
+module Pos = struct
+      type t =
+            | Black
+            | White
+            | Outer
 
-let pos_of_int = function
-      | num when num = FlatGraph.root -> FlatGraph.(root, root)
-      | num -> (num mod 6, num / 6)
+      let int_of_pos (x, y) =
+            if x < 0 || x > bound || y < 0 || y > bound
+            then -1
+            else x + y * size
 
-type value =
-      | Outer
-      | White
-      | Black
+      let left (x, y) = (x - 1, y)
+      let top (x, y) = (x, y - 1)
 
-let value_of_pos (x, y) =
-      try 
-            let row = List.nth input x in
-            match List.nth row y with
-            | 0 -> White
-            | 1 -> Black
-            | _ -> Outer
-      with
-            | Failure _ -> Outer
-            | Invalid_argument _ -> Outer
+      let t_of_pos (x, y) =
+            try
+                  let row = List.nth input y in
+                  match List.nth row x with
+                  | 1 -> Black
+                  | 0 -> White
+                  | _ -> White
+            with
+                  | _ -> Outer
+end
 
-let flat_relation_graph =
-      let xs = List.init (7 * 7) (fun x -> x mod 7) in
-      let ys = List.init (7 * 7) (fun x -> x / 7) in
+let connections_graph =
+      let xs = List.init (size_ * size_) (fun index -> index mod size_) in
+      let ys = List.init (size_ * size_) (fun index -> index / size_) in
+      
+      List.fold_left2 (fun graph x y ->
+            let cur = (x, y) in
+            let left = (x, y) |> Pos.left in
+            let top = (x, y) |> Pos.top in
 
-      List.fold_left2 (fun acc x y ->
-            let left_pos = (x-1, y) in
-            let curr_pos = (x, y) in
-            let upper_pos = (x, y-1) in
+            let _ = cur |> Pos.int_of_pos |> string_of_int |> print_endline in
 
-            let tuple_of_points pos_a pos_b graph =
-                  match value_of_pos pos_a, value_of_pos pos_b with
-                  | (Outer, Black)
-                  | (Black, Black) -> FlatGraph.add (int_of_pos pos_a) (int_of_pos pos_b) graph
-                  | (Black, Outer) -> FlatGraph.add (int_of_pos pos_b) (int_of_pos pos_a) graph
-                  | _ -> graph
+            let conn a b = match Pos.(t_of_pos a, t_of_pos b) with
+                  | (Outer, Black) -> ListGraph.add Pos.(int_of_pos a, int_of_pos b) []
+                  | (Black, Black)
+                  | (Black, Outer) -> ListGraph.add Pos.(int_of_pos b, int_of_pos a) []
+                  | _ -> ListGraph.id
             in
-            acc
-            |> tuple_of_points left_pos curr_pos
-            |> tuple_of_points curr_pos upper_pos
-      ) FlatGraph.empty xs ys
+            graph
+            |> conn left cur
+            |> conn top cur
 
-let res_list = FlatGraph.list_of_graph [FlatGraph.root] flat_relation_graph
+      ) ListGraph.empty xs ys
 
-let result = 
-      let (<&&>) = List.init in
-      6 <&&> fun x ->
-      6 <&&> fun y ->
-            let result_value = function
-                  | int_pos when List.mem int_pos res_list -> 1
-                  | _ -> 0
-            in
-            (x, y)
-            |> int_of_pos
-            |> result_value
+let last_connections = ListGraph.last_items connections_graph
 
-(*
-let rec ger_result input = function
-      | TreeGraph.End -> input
-      | TreeGraph.Node (key, inner, next) ->
-*)
-
-(* convert tree graph to board OR override input board *)
+let result =
+      List.init size (fun y ->
+      List.init size (fun x ->
+            if List.mem (Pos.int_of_pos (x, y)) last_connections
+            then 1
+            else 0
+      ))
 
